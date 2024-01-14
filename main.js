5aba725f08bb4da2ee6cb4663d92323e9d55ae24
kevinelong <kevinelong@gmail.com>
2024-01-14 14:26:43 -0800

    WIP refactor

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+const NEW_LINE = "\n";
+const EMPTY = "";
+const CONTENT = { "&#127942;&nbsp;Tournaments": `<a href="./bracket/">Bracket</a>` };
+
+
+const getContent = (s) => CONTENT[s] ? CONTENT[s] : "COMING SOON";
 const safe = (name) => encodeURI(name.toLowerCase());
-const page = (name) =>
-    `<div class="page ${safe(name)}" id="${safe(name)}">
-<div class="title">${link("&lt;&nbsp;" + name, "#app")}</div>
-${getContent(name)}
-</div>`;
 const link = (s, h) => `<a href="${h ? h : "#" + safe(s)}" class="link">${s}</a>`;
 const list = s => s.split(NEW_LINE).map(s => s.trim()).filter(s => s.length > 0);
 
-const NEW_LINE = "\n";
-const EMPTY = "";
+const page = (name) => `
+    <div class="page ${safe(name)}" id="${safe(name)}">
+        <div class="title">${link("&lt;&nbsp;" + name, "#app")}</div>
+        ${getContent(name)}
+    </div>
+`;
+
 const PAGES = list(`
 &#127968;&nbsp;Home
 &#128274;&nbsp;Login/Logout
@@ -18,14 +24,11 @@ const PAGES = list(`
 &#128176;&nbsp;Auctions
 `);
 
-const CONTENT = {
-    "&#127942;&nbsp;Tournaments" : `<a href="./bracket/">Bracket</a>`
-}
-function getContent(s){
-    const c = CONTENT[s];
-    return c ? c : "COMING SOON";
-}
-let html = `<nav>` + PAGES.map(s => link(s)).join(EMPTY) + 
-`<div id="logo"><img src="ace-shot-logo.png"><br>Premium Tournament Direction<br>&amp;<br>Calcutta Auctions</div>` + `</nav>`;
-html += `<content>` + PAGES.map(s => page(s)).join(EMPTY) + `</content>`;
-document.body.innerHTML = html;
+document.body.innerHTML = `
+<nav>
+    ${PAGES.map(s => link(s)).join(EMPTY)} 
+    <div id="logo"><img src="ace-shot-logo.png"><br>Premium Tournament Direction<br>&amp;<br>Calcutta Auctions</div>
+</nav>
+<content>
+    ${PAGES.map(s => page(s)).join(EMPTY)}
+</content>`;
